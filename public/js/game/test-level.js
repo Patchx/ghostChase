ddaa57a437bb27fe9de5269e5298f2b5deb26c0f
Patchx <robert.anderson.fl@gmail.com>
2016-05-13 15:48:24 -0400

    added background image to game

--- public/js/game/test-level.js
+++ public/js/game/test-level.js
@@ -4,9 +4,11 @@ var divY = $("#game").width() / 1.5;
 var game = new Phaser.Game(divX, divY, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('background', './images/game-background.png');
 }
 
 function create() {
+  game.add.sprite(0, 0, 'background');
 }
 
 function update() {
