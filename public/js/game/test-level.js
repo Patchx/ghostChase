97e311b4c61e28e837f5473eefd64be2de7629e2
Patchx <robert.anderson.fl@gmail.com>
2016-05-13 16:09:52 -0400

    added player1 sprite

--- public/js/game/test-level.js
+++ public/js/game/test-level.js
@@ -5,10 +5,12 @@ var game = new Phaser.Game(divX, divY, Phaser.AUTO, 'game', { preload: preload, 
 
 function preload() {
   game.load.image('background', './images/game-background.png');
+	game.load.spritesheet('player1', './images/player1-sprite.png', 49, 122);
 }
 
 function create() {
   game.add.sprite(0, 0, 'background');
+	player = game.add.sprite(game.world.width - 150, game.world.height - 150, 'player1');
 }
 
 function update() {
