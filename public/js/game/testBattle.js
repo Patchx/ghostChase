17a87d29a2158235471d82644691845c4be3cf50
Patchx <robert.anderson.fl@gmail.com>
2016-05-28 17:01:47 -0400

    site works on heroku!

--- public/js/game/testBattle.js
+++ public/js/game/testBattle.js
@@ -8,7 +8,7 @@ preload.prototype = {
 	create: function(){
 	  this.game.add.sprite(0, 0, 'background');
 
-		player1 = game.add.sprite(game.world.width - 150, game.world.height - 150, 'player1');
+		player1 = game.add.sprite(game.world.width * 0.5, game.world.height * 0.5, 'player1');
 		player1.animations.add('dodge-b', [0, 1, 0], 10, false);
 
 		rArrow = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
@@ -37,5 +37,5 @@ function dodgeUp() {
 
 function returnPos() {
 	var tween = game.add.tween(arguments[0]);
-	tween.to({ x: game.world.width - 150, y: game.world.height - 150 }, 100, Phaser.Easing.Linear.None, true, 150);
+	tween.to({ x: game.world.width * 0.5, y: game.world.height * 0.5 }, 100, Phaser.Easing.Linear.None, true, 150);
 }
