659261b5a542c3511c698a607c115d64a8641f5d
Patchx <robert.anderson.fl@gmail.com>
2016-05-30 15:30:49 -0400

    fluid attack and dodge animations and have a working pause btn

--- public/js/game/testBattle.js
+++ public/js/game/testBattle.js
@@ -13,60 +13,116 @@ preload.prototype = {
 
 	  this.game.add.sprite(0, 0, 'background');
 
-		player1 = game.add.sprite(game.world.width * 0.5, game.world.height * 0.5, 'player1');
+		player1 = game.add.sprite(game.world.width * 0.5, game.world.height * 0.5, 'player1', 9);
 		player1.scale.setTo(2.5, 2.5);
 
 		// Animation Settings
 
-		playerSpeed = 10;
-		playerDist = 20;
-		player1.animations.add('dodge', [10, 6, 10], playerSpeed, false);
-		player1.animations.add('attack', [10, 12, 10], playerSpeed, false);
-		player1.events.onAnimationComplete.add(function(){
-			returnPos(player1);
-		},this);
+		playerSpeed = 6;
+		playerDist = 30;
+
+		player1.animations.add('dodge', [9,10], playerSpeed, false);
+		player1.animations.add('attack1', [9,12], playerSpeed, false);
+		player1.animations.add('attack2', [9,12,13], playerSpeed, false);
+		player1.animations.add('attack3', [13,14,13], playerSpeed, false);
 
 		// Controls
 
 		backArrow = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-		backArrow.onDown.add(dodge, {dir: 'back'});
 		upArrow = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-		upArrow.onDown.add(dodge, {dir: 'up'});
 		downArrow = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-		downArrow.onDown.add(dodge, {dir: 'down'});
-
 		frontArrow = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-		frontArrow.onDown.add(attack, this);
+
+		backArrow.onDown.add(dodge, {dir: 'back'});
+		upArrow.onDown.add(dodge, {dir: 'up'});
+		downArrow.onDown.add(dodge, {dir: 'down'});
+		frontArrow.onDown.add(attack1, this);
+
+		pauseBtn = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		pauseBtn.onDown.add(pause);
 	},
 
 	update: function(){
 	}
 }
 
+function pause() {
+	if( game.paused !== true) {
+		game.paused = true;
+	} else {
+		game.paused = false;
+	}
+}
+
+function enableFighter() {
+	enableDodge();
+	frontArrow.enabled = true;
+}
+
+function enableDodge() {
+	backArrow.enabled = true;
+	upArrow.enabled = true;
+	downArrow.enabled = true;
+}
+
+function disableDodge() {
+	backArrow.enabled = false;
+	upArrow.enabled = false;
+	downArrow.enabled = false;
+}
+
 function returnPos() {
 	var tween = game.add.tween(arguments[0]);
 	tween.to({ x: game.world.width * 0.5, y: game.world.height * 0.5 }, playerSpeed, Phaser.Easing.Linear.None, true, 150);
+	player1.frame = 9;
+	enableFighter();
 }
 
 function dodge(dir) {
+	disableDodge();
+	frontArrow.enabled = false;
+
 	var tweenDodge = game.add.tween(player1);
 	switch (this.dir) {
 		case 'up':
-			var dodgeHash = { y: player1.y - playerDist, repeatDelay: 3000 };
+			var dodgeHash = { y: player1.y - playerDist};
 			break;
 		case 'down':
-			var dodgeHash = { y: player1.y + playerDist, repeatDelay: 3000 };
+			var dodgeHash = { y: player1.y + playerDist};
 			break;
 		case 'back':
-			var dodgeHash = { x: player1.x + playerDist, repeatDelay: 3000 };
+			var dodgeHash = { x: player1.x + playerDist};
 			break;
 	}
 	player1.animations.play('dodge');
 	tweenDodge.to(dodgeHash, playerSpeed, Phaser.Easing.Linear.None, true, 0);
+
+	player1.events.onAnimationComplete.add(function(){
+		returnPos(player1);
+	},this);
 }
 
-function attack() {
+function attack1() {
+	disableDodge();
+
 	var tweenAttack = game.add.tween(player1);
-	tweenAttack.to({ x: player1.x - playerDist, repeatDelay: 3000 }, playerSpeed, Phaser.Easing.Bounce.In, true, 0);
-	player1.animations.play('attack');
+	tweenAttack.to({ x: player1.x - playerDist, repeatDelay: 3000 }, playerSpeed, Phaser.Easing.Linear.None, true, 0);
+	player1.animations.play('attack1');
+	frontArrow.onDown.addOnce(attack2, this);
+
+	player1.events.onAnimationComplete.add(function(){
+		returnPos(player1);
+	},this);
+}
+
+function attack2() {
+	player1.animations.play('attack2');
+	frontArrow.onDown.addOnce(attack3, this);
+}
+
+function attack3() {
+	frontArrow.enabled = false;
+	var tweenAttack = game.add.tween(player1);
+	tweenAttack.to({ x: player1.x - (playerDist * 0.5), repeatDelay: 3000 }, playerSpeed, Phaser.Easing.Linear.None, true, 0);
+	player1.animations.play('attack3');
 }
